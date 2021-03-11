666ce9f4142ac50c769fa7d580c000cc4def9c7c
SQZ777 <darren_zhang@trendmicro.com>
2021-03-11 12:54:13 +0800

    add talkService test case

---
+++ TestAWSLambdaAndDI/TestTalkService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestAWSLambdaAndDI
+{
+    [TestClass]
+    public class TestTalkService
+    {
+        [TestMethod]
+        public void Input_Darren_Should_Return_Darren_hello()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
